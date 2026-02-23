af767ebb26ad3bde84cb0856b7073f3d0ee53687
Ajeet Gupta <gajeet031@gmail.com>
2026-02-24 03:21:05 +0530

    romanToInt

---
+++ array/romanToInt.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int romanToInt(string s) {
+
+        unordered_map<char, int> value = {
+            {'I', 1}, {'V', 5}, {'X', 10},
+            {'L', 50}, {'C', 100},
+            {'D', 500}, {'M', 1000}
+        };
+
+        int total = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            // If current value < next value → subtract
+            if (i + 1 < s.length() && value[s[i]] < value[s[i + 1]]) {
+                total -= value[s[i]];
+            }
+            else {
+                total += value[s[i]];
+            }
+        }
+
+        return total;
+    }
+};
+
+int main() {
+
+    Solution obj;
+
+    string roman = "MCMXCIV";
+
+    cout << obj.romanToInt(roman) << endl;
+
+    return 0;
+}
